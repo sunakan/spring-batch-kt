3b50a214f35a217d50bc7db3cdbc344e0e4fb5be
sunakan <sunakan03@gmail.com>
2021-01-12 16:53:51 +0900

    taskletで別のクラスcallしたり

--- 06-callable-tasklet/src/main/kotlin/com/example/callable_tasklet/CallableTaskletApplication.kt
+++ 06-callable-tasklet/src/main/kotlin/com/example/callable_tasklet/CallableTaskletApplication.kt
@@ -5,8 +5,11 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter
+import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -17,9 +20,10 @@ import java.util.concurrent.Callable
 class CallableTaskletApplication {
 
     @Bean
-    fun job1(jobBuilderFactory: JobBuilderFactory, step1: Step): Job {
+    fun job1(jobBuilderFactory: JobBuilderFactory, step1: Step, step2: Step): Job {
         return jobBuilderFactory.get("basicjob")
             .start(step1)
+            .next(step2)
             .build()
     }
 
@@ -38,7 +42,7 @@ class CallableTaskletApplication {
     }
 
     @Bean
-    fun callableObject(): Callable<RepeatStatus>  {
+    fun callableObject(): Callable<RepeatStatus> {
         return Callable<RepeatStatus> {
             println("=====================================別スレッド")
             println("HelloWorld")
@@ -46,10 +50,45 @@ class CallableTaskletApplication {
         }
     }
 
+    @Bean
+    fun step2(stepBuilderFactory: StepBuilderFactory): Step {
+        return stepBuilderFactory.get("step2")
+            .tasklet(tasklet2(null))
+            .build()
+    }
+
+    @StepScope
+    @Bean
+    fun tasklet2(@Value("#{jobParameters['message']}") message: String?): MethodInvokingTaskletAdapter {
+        val methodInvokingTaskletAdapter = MethodInvokingTaskletAdapter()
+        methodInvokingTaskletAdapter.setTargetObject(service())
+        // 文字列でメソッド名記述とか。。。orz
+        methodInvokingTaskletAdapter.setTargetMethod("serviceMethod")
+
+        println("----jobParameters['message']")
+        println(message)
+        println("----")
+        methodInvokingTaskletAdapter.setArguments(arrayOf(message))
+        return methodInvokingTaskletAdapter
+    }
+
+    @Bean
+    fun service(): CustomService {
+        return CustomService()
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             runApplication<CallableTaskletApplication>(*args)
         }
+    }
+}
+
+class CustomService {
+    fun serviceMethod(message: String) {
+        println("==========================")
+        println("service method's args ==> $message")
+        println("==========================")
     }
 }
--- 06-callable-tasklet/src/test/kotlin/com/example/callable_tasklet/CallableTaskletApplicationTests.kt
+++ 06-callable-tasklet/src/test/kotlin/com/example/callable_tasklet/CallableTaskletApplicationTests.kt
@@ -9,5 +9,4 @@ class CallableTaskletApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
